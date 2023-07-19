5124765a1bbb6e7b2184d6d1098891f0cdb7f490
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-19 10:52:38 +0100

    added files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,35 @@
 // Header.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <header className="header">
       <div className="header__left">
         <h1 className="header__logo">Uber</h1>
         <div className="header__links">
-          <a href="#company">Company</a>
-          <a href="#safety">Safety</a>
-          <a href="#help">Help</a>
+          <button onClick={toggleDropdown}>Company</button>
+          {showDropdown && (
+            <div className="header__dropdown">
+              <a href="#about">About us</a>
+              <a href="#offerings">Our offerings</a>
+              <a href="#how-uber-works">How Uber works</a>
+              <a href="#global-citizenship">Global citizenship</a>
+              <a href="#newsroom">Newsroom</a>
+              <a href="#investors">Investor relations</a>
+              <a href="#blog">Blog</a>
+              <a href="#careers">Careers</a>
+            </div>
+          )}
+          <div>Safety</div>
+          <div>Help</div>
+          
+
         </div>
       </div>
 
